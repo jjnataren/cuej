2b1db192c157a3ed106c8a70c3f47c8cbf15ed1c
jjnataren <jesus.nataren@gmail.com>
2019-04-24 10:19:01 -0500

    captacion base

--- juridicos/SICUEJ/captacion/nuevo.php
+++ juridicos/SICUEJ/captacion/nuevo.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 	-- ==============================================================================
@@ -49,18 +48,24 @@ else
 
 include ("../php/HTML.php");
 
+
+
+$sql = "SELECT cap.*,
+                    (Select car.carrera from carreras car
+                            where car.id_carrera =  cap.id_topico_interes limit 1) as carrera
+                                FROM sicuej.captacion cap where id_empleado = $id_usuario;";
+
+$results = mysqli_query($conexion,$sql);
+
+
 ?>
-
-
-
-
 <script type="text/javascript" src="../js/flexigrid.pack.js"></script>
 <script type="text/javascript" src="../js/Funciones_Jquery_Captacion.js"></script>
 <script type="text/javascript" >
 
 	$('Body').ready(function(){
 
-		var table = 	 $('#tbl_captacion').DataTable();
+		$('#tbl_captacion').DataTable();
 
 	/*	Captacion_B
 	uscar();
@@ -68,56 +73,26 @@ include ("../php/HTML.php");
 		$('#Btn_Buscar').click(function(){
 			Captacion_Buscar();
 		});
-		*/
-		$('#Btn_Nuevo').click(function(){
-			Captacion_Nuevo();
-		});
 
-
-		$('#Btn_Update').click(function(){
-			Captacion_Nuevo();
-		});
-
-
-		$('#tbl_captacion tbody').on( 'click', 'tr', function () {
-	        $(this).toggleClass('selected');
-	    } );
-
-
-		$('#btnContactar').click(function(){
-
-			contactar(JSON.stringify(table.rows('.selected').data()) );
-
-		});
-
-
+		$('#Btn_Nuevo_Alumno').click(function(){
+			Captacion_Buscar();
+		});*/
 	});
 
 </script>
 
 <?php include ("../php/Body.php"); ?>
 
-<?php
-
-$sql = "SELECT cap.*,
-                    (Select car.carrera from carreras car
-                            where car.id_carrera =  cap.id_topico_interes limit 1) as carrera,
-                                 (Select pa.paisnombre from pais pa where pa.id =  cap.pais limit 1) as paisdesc,
-                                    (SELECT edo.estadonombre FROM estado edo where edo.id =  cap.estado limit 1) as estadodesc
-                                FROM sicuej.captacion cap where id_empleado = $id_usuario;";
-mysqli_query($conexion, "SET NAMES 'utf8'");
-
-$results = mysqli_query($conexion,$sql);
-
-?>
-
 <table  width="100%" align="center">
 	<thead>
 		<tr>
-			<th  colspan="4">Procesos de captación <div class="header_01"><hr /></div></th>
+			<th  colspan="4">Nuevo proceso de captación <div class="header_01"><hr /></div></th>
 		</tr>
 	</thead>
 </table>
+
+
+<form>
 	<table width="100%" align="center">
 		<tr>
 			<td>
@@ -128,10 +103,10 @@ $results = mysqli_query($conexion,$sql);
 	if($_SESSION["Insert"] == 1)
 	{
 ?>
+				<a href="nuevo" class="button"  >
+					Guardar
 
-
-				<input class="button" type="button" id = "Btn_Nuevo" name = "Btn_Nuevo"  value = "Nuevo" />
-				<input class="button" type="button" id = "btnContactar" name = "Btn_Nuevo"  value = "Contactar" />
+				</a>
 <?php
 	}
 ?>
@@ -139,23 +114,20 @@ $results = mysqli_query($conexion,$sql);
 		</tr>
 	</table>
 <p>&nbsp;</p>
-<div id="div_captacion">
-<div class="table-responsive">
-	<table id="tbl_captacion" class="table table-sm table-hover" style="width:100%">
+<div id="div_nuevo">
+
+	<table id="tbl_captacion" class="display table-condensed table-striped" style="width:100%">
 		<thead>
 		<tr>
 			<th>Id</th>
-			<th>Alta</th>
-			<th>Estatus</th>
+			<th>Fecha captura</th>
 			<th>Cliente</th>
-
-			<th>Correo</th>
-			<th>Tel</th>
-			<th>Social</th>
-			<th>Interes</th>
+			<th>Correo electronico</th>
+			<th>Telefono</th>
+			<th>Medio de contacto</th>
+			<th>Topico de interes</th>
+			<th>País</th>
 			<th>Estado</th>
-
-
 		</tr>
 
 		</thead>
@@ -165,46 +137,20 @@ $results = mysqli_query($conexion,$sql);
 			<?php
 
 			while ($row = @mysqli_fetch_assoc($results))
-
-			{
-
-			    $estatusNuevo = "";
-			    $icon = "fa fa-calendar";
-
-			    switch ($row["estatus"]){
-
-			        case "1":
-			            $estatusNuevo = "registrado";
-			            break;
-			        case "2":
-			            $estatusNuevo = "contactado";
-			            $icon = "fa fa-calendar-check-o";
-			            break;
-			        default:
-			            $estatusNuevo = "desconocido";
-
-			    }
-
-			    ?>
+			{?>
 
 
 
 			<tr>
-				<td><a href="javascript: Captacion_Datos(<?php echo $row['id'];?>)"><?php echo $row["id"]; ?></a> </td>
+				<td><?php echo $row["id"]; ?></td>
 				<td><?php echo $row["captacion_fecha_alta"]; ?></td>
-				<td>
-					<?php echo  $estatusNuevo; ?>
-					<i class="<?php echo $icon;?>"></i>
-
-				</td>
 				<td><?php echo $row["cliente_nombre"]; ?></td>
-
 				<td><?php echo $row["cliente_correo_electronico"]; ?></td>
 				<td><?php echo $row["cliente_telefono"]; ?></td>
 				<td><?php echo $row["medio_contacto"]; ?></td>
 				<td><?php echo $row["carrera"]; ?></td>
-				<td><?php echo $row["estadodesc"]; ?></td>
-
+				<td><?php echo $row["pais"]; ?></td>
+				<td><?php echo $row["estado"]; ?></td>
 
 
 			</tr>
@@ -217,7 +163,7 @@ $results = mysqli_query($conexion,$sql);
 		</tbody>
 
 	</table>
-</div>
+
 </div>
 <br />
 <br />
