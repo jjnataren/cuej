9422c12d5ae9351e6a568126b00769db24e9b16a
jjnataren <jesus.nataren@gmail.com>
2019-09-26 23:21:14 -0500

    round creditos

--- juridicos/SICUEJ/ajax/Servicios_Escolares/Listas_Egresados_Buscar.php
+++ juridicos/SICUEJ/ajax/Servicios_Escolares/Listas_Egresados_Buscar.php
@@ -1,29 +1,29 @@
 <?php
 /*
-	-- ==============================================================================
-	-- Empresa: Centro Universitario de Estudios Jurídicos
-	-- Proyecto: Sistema Integral - Administrativo
-	-- Autor: Julio César Morales Crispín
-	-- Responsable: Nancy Flores Torrecilla
-	-- Fecha de Creación: [Octubre, 19 2017]	
-	-- País: México
-	-- Objetivo: Busqueda de alumnos egresados por carrera y plan de estudios
-	-- Última Modificación: [Octubre, 19 2017]
-	-- Realizó:Julio César Morales Crispín
-	-- Observaciones: Creación de Archivo
-	-- ===============================================================================
-*/
+ -- ==============================================================================
+ -- Empresa: Centro Universitario de Estudios Jurídicos
+ -- Proyecto: Sistema Integral - Administrativo
+ -- Autor: Julio César Morales Crispín
+ -- Responsable: Nancy Flores Torrecilla
+ -- Fecha de Creación: [Octubre, 19 2017]
+ -- País: México
+ -- Objetivo: Busqueda de alumnos egresados por carrera y plan de estudios
+ -- Última Modificación: [Octubre, 19 2017]
+ -- Realizó:Julio César Morales Crispín
+ -- Observaciones: Creación de Archivo
+ -- ===============================================================================
+ */
 session_start();
-	
+
 include ("../../php/Funciones.php");
 
 $sql_alumnos = "SELECT id_alumno_programa, apellido_paterno, apellido_materno, nombre, cuenta, creditos FROM alumnos JOIN alumnos_programas USING (id_alumno) JOIN planes_estudio USING (id_plan_estudio) WHERE id_carrera = '".$_POST["id_Carrera"]."' AND id_plan_estudio = '".$_POST["id_Plan_Estudio"]."' ORDER BY apellido_paterno, apellido_materno, nombre;";
-$resultado_alumnos = mysqli_query($conexion, $sql_alumnos);	
+$resultado_alumnos = mysqli_query($conexion, $sql_alumnos);
 $registros_alumnos = @mysqli_num_rows($resultado_alumnos);
 
 if($registros_alumnos > 0)
-	{
-?>
+{
+    ?>
 <table class="cuej" width="100%">
   <thead>
     <tr>
@@ -36,21 +36,21 @@ if($registros_alumnos > 0)
   <tbody>
 <?php
 		$i = 0;
-		
+
 		while($fila_alumnos = mysqli_fetch_array($resultado_alumnos))
 			{
 				$sql_alumnos_historial = "SELECT SUM(creditos) AS creditos FROM materias JOIN alumnos_historial USING (id_materia) WHERE id_alumno_programa = '".$fila_alumnos["id_alumno_programa"]."' AND (calificacion = 6 || calificacion = 7 || calificacion = 8 || calificacion = 9 || calificacion = 10);";
 				$resultado_alumnos_historial = mysqli_query($conexion, $sql_alumnos_historial);
 				$fila_alumnos_historial = mysqli_fetch_array($resultado_alumnos_historial);
-				
-				if ($fila_alumnos["creditos"] == $fila_alumnos_historial["creditos"])
+
+				if ($fila_alumnos["creditos"] == round($fila_alumnos_historial["creditos"],2))
 					{
 ?>
     <tr>
       <td class="cuej" align="center"><?php echo ++$i; ?></td>
       <td class="cuej" align="center"><?php echo $fila_alumnos["cuenta"]; ?></td>
       <td class="cuej"><?php echo utf8_encode($fila_alumnos["apellido_paterno"]." ".$fila_alumnos["apellido_materno"]." ".$fila_alumnos["nombre"]); ?></td>
-      <td class="cuej" align="center"><?php echo $fila_alumnos_historial["creditos"] ." de ". $fila_alumnos["creditos"]; ?></td>
+      <td class="cuej" align="center"><?php echo round( $fila_alumnos_historial["creditos"] ,2) ." de ". $fila_alumnos["creditos"]; ?></td>
     </tr>
 <?php
 					}
@@ -63,7 +63,7 @@ if($registros_alumnos > 0)
     </tr>
   </tfoot>
 </table>
-     
+
 <p align="center">
 
 <form name="Formulario" id="Formulario" action="../impresiones/Servicios_Escolares/Listas_Egresados_XLS.php" method="POST" target="_blank" >
